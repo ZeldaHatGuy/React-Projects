2aa9b343ccc8a64b492a09917e7e7cb164da20e6
ZeldaHatGuy <Equin0xx@yahoo.com>
2023-07-25 22:00:25 -0500

    adding more features to popcorn app

--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 function Test() {
@@ -17,12 +17,13 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
+    {/* <StarRating
       maxRating={3}
       messages={["decent", "good", "great"]}
       color="blue"
     />
-    <Test />
+    <Test /> */}
+    <App />
   </React.StrictMode>
 );
 
